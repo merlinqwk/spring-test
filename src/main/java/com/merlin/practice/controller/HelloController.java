ca6fee35500bc677e33abab298962e800ae000c0
merlin <2451304828@qq.com>
2018-02-23 15:20:04 +0800

    对象返回json

--- src/main/java/com/merlin/practice/controller/HelloController.java
+++ src/main/java/com/merlin/practice/controller/HelloController.java
@@ -1,15 +1,27 @@
 package com.merlin.practice.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.merlin.practice.model.Student;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
-
+    @ResponseBody
     @PostMapping("hello")
-    public String test(){
-        return "hello";
+    public String test(@RequestBody Student student){
+        StringBuffer sb = new StringBuffer();
+        sb.append("id:" + student.getId());
+        sb.append("; name:" + student.getName());
+        sb.append("; sex:" + student.getSex());
+        return String.valueOf(sb);
+    }
+
+    @ResponseBody
+    @PostMapping("getStudent")
+    public Student getStudent(){
+        Student student = new Student(1,"Jack","boy");
+        return student;
     }
 }
---
+++ src/main/java/com/merlin/practice/model/Student.java
@@ -0,0 +1,45 @@
+package com.merlin.practice.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by qwk on 2018-02-07 10:18
+ **/
+public class Student implements Serializable{
+
+    private static final long serialVersionUID = -669259594768650851L;
+
+    private Integer id;
+    private String name;
+    private String sex;
+
+    public Student(Integer id, String name, String sex) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
